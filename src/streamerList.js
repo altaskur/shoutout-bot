5ecb2eb7b0f496da9b0eff76ff7175a3737aeb33
altaskur <altaskur@gmai.com>
2023-04-12 22:12:01 +0200

    update stremers list

--- src/streamerList.js
+++ src/streamerList.js
@@ -1,3 +1,3 @@
-const streamerList = ['raupulus', 'danirod_', 'altaskur', 'hellsing2030', 'afor_digital', 'pythonesa', 'manzdev', 'todocode', 'niv3k_el_pato', 'ckmu32', 'cursosdedesarrollo', 'nurialiano', 'padawanstrainer', 'viciostv'];
+const streamerList = ['raupulus', 'danirod_', 'altaskur', 'hellsing2030', 'afor_digital', 'pythonesa', 'manzdev', 'todocode', 'niv3k_el_pato', 'ckmu32', 'cursosdedesarrollo', 'nurialiano', 'padawanstrainer', 'viciostv', 'programacion_es'];
 
 module.exports = streamerList;
