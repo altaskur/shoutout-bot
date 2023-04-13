f2b3c77e03d4617e27dc4692342a9ad23c305124
altaskur <altaskur@gmai.com>
2023-04-13 23:39:15 +0200

    update streamerList

--- src/main.js
+++ src/main.js
@@ -101,6 +101,5 @@ client.on('chat', (channel, user, message, self) => {
 
   if (shoutStatus) checkUser(user['display-name']);
 });
-const buttons = document.querySelectorAll('input[type=button]');
 
 client.connect();
